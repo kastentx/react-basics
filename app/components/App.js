f50dc3d61fa1dd94166438274c70754060904b38
Nick Kasten <kastencode@gmail.com>
2016-08-25 01:27:56 -0500

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+    render: function() {
+        return (
+        	<div>
+	        	<h1>HELLO WORLD</h1>
+        	</div>	
+        )
+    }
+});
+
+module.exports = App;
