0073819531abc17385001e4fb86cea818a4b21c2
Nick Kasten <kastencode@gmail.com>
2016-08-25 13:06:14 -0500

    second commit. added a second, 'stateful' component.

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,12 @@
 var React = require('react');
+var SecondThing = require('./SecondThing');
 
 var App = React.createClass({
     render: function() {
         return (
         	<div>
 	        	<h1>HELLO WORLD</h1>
+	        	<SecondThing />
         	</div>	
         )
     }
